5492a87020fe18bdd25158b26fb70f05bd138ff5
Justin Tabinski <justin_1791@hotmail.com>
2021-06-14 12:09:37 -0500

    added current weather funct

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const { default: gulpfile } = require("../gulpfile");
+
 const apiKey = 'cc42663f6c57ef798f9e29193e9b86f4';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/';
 const unitsParam = 'units=metric';
@@ -5,6 +7,14 @@ const unitsParam = 'units=metric';
 const getFiveDay = async (lat, lon) => {
   const response = await fetch(
     `${baseUrl}forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&${unitsParam}`
+  );
+  const data = await response.json();
+  return data;
+};
+
+const getCurrentWeather = async (lat, lon) => {
+  const response = await fetch(
+    `${baseUrl}weather?lat=${lat}&lon=${lon}&appid=${apiKey}&${unitsParam}`
   );
   const data = await response.json();
   return data;
